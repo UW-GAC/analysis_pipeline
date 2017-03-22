1e8a64111424f8bb435cd473e5bca503eefbcea2
Stephanie M. Gogarten <sdmorris@uw.edu>
2017-03-22 16:36:43 -0700

    Combine segment-related code in one file

--- TopmedPipeline/R/segments.R
+++ TopmedPipeline/R/segments.R
@@ -14,3 +14,21 @@ writeSegmentFile <- function(segments, file) {
         select_(~chromosome, ~start, ~end)
     write.table(seg.df, file=file, quote=FALSE, sep="\t", row.names=FALSE)
 }
+
+getSegments <- function(file) {
+    dat <- read.table(file, header=TRUE, sep="\t", stringsAsFactors=FALSE)
+    GRanges(seqnames=dat$chromosome,
+            ranges=IRanges(start=dat$start, end=dat$end))
+}
+
+# return the elements of varList where the first variant is in the segment
+subsetBySegment <- function(varList, segment, segment.file) {
+    # create a GRanges object containing the first variant from each item in varList
+    dat <- do.call(rbind, lapply(varList, function(x) x[1,]))
+    gr <- GRanges(seqnames=dat$chromosome,
+                  ranges=IRanges(start=dat$position, end=dat$position))
+    
+    segments <- getSegments(segment.file)
+    ind <- queryHits(findOverlaps(gr, segments[segment]))
+    varList[ind]
+}
