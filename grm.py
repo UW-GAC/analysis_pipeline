9ce840b27f05b0db4d48104c923f412206011134
Stephanie M. Gogarten <sdmorris@uw.edu>
2017-05-18 11:51:19 -0700

    allow multiple cores for GRM

--- grm.py
+++ grm.py
@@ -17,6 +17,8 @@ parser.add_argument("--cluster_type", default="uw",
                     help="type of compute cluster environment [default %(default)s]")
 parser.add_argument("--cluster_file", default=None, 
                     help="file containing options to pass to the cluster (sge_request format)")
+parser.add_argument("-n", "--ncores", default="1-8",
+                    help="number of cores to use; either a number (e.g, 1) or a range of numbers (e.g., 1-4) [default %(default)s]")
 parser.add_argument("-e", "--email", default=None,
                     help="email address for job reporting")
 parser.add_argument("--print_only", action="store_true", default=False,
@@ -26,6 +28,7 @@ args = parser.parse_args()
 configfile = args.config_file
 cluster_file = args.cluster_file
 cluster_type = args.cluster_type
+ncores = args.ncores
 email = args.email
 print_only = args.print_only
 
@@ -49,4 +52,4 @@ config["out_file"] = configdict["out_prefix"] + "_grm.RData"
 configfile = configdict["out_prefix"] + "_" + job + ".config"
 TopmedPipeline.writeConfig(config, configfile)
 
-jobid[job] = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile], email=email, print_only=print_only)
+jobid[job] = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile], request_cores=ncores, email=email, print_only=print_only)
