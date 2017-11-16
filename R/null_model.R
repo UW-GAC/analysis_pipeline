9e512fb581e9a26c435bf901ebaaea8ea02bd531
Stephanie M. Gogarten <sdmorris@uw.edu>
2017-11-15 16:22:33 -0800

    use fitNullModel2
    Replace fitNullMM and fiNullReg with fitNullModel2, which covers
    both cases. Replace inverse normal code with updateNullModelOutcome
    (not tested yet, should be replaced by wrapper function in genesis2).

--- R/null_model.R
+++ R/null_model.R
@@ -1,7 +1,7 @@
 library(argparser)
 library(TopmedPipeline)
 library(Biobase)
-library(GENESIS)
+library(genesis2)
 library(gdsfmt)
 sessionInfo()
 
@@ -24,7 +24,7 @@ optional <- c("gds_file"=NA, # required for conditional variants
               "n_pcs"=3,
               "out_file"="null_model.RData",
               "rescale_variance"="marginal",
-              "resid_covars"=TRUE,
+              #"resid_covars"=TRUE,
               "sample_include_file"=NA)
 config <- setConfigDefaults(config, required, optional)
 print(config)
@@ -53,57 +53,33 @@ random <- if (!is.na(config["pcrelate_file"])) "kinship" else if (!is.na(config[
 model.string <- modelString(outcome, covars, random, group.var)
 message("Model: ", model.string)
 
-if (!is.null(grm)) {
-    
-    ## fit null model allowing heterogeneous variances among studies
-    nullmod <- fitNullMM(annot, outcome=outcome, covars=covars,
-                         covMatList=grm, scan.include=sample.id,
+## fit null model allowing heterogeneous variances among studies
+nullmod <- fitNullModel2(annot, outcome=outcome, covars=covars,
+                         covMatList=grm, sample.id=sample.id,
                          family=family, group.var=group.var)
 
-    ## if we need an inverse normal transform, take residuals and refit null model
-    if (as.logical(config["inverse_normal"])) {
-        if (is.null(group.var)) {
-            annot <- addInvNorm(annot, nullmod, outcome, covars)
-        } else {
-            groups <- unique(annot[[group.var]])
-            ## inverse-normal transform residuals from each study separately (mean=0, var=1)
-            resid.group <- do.call(rbind, lapply(groups, function(g) {
-                samp.g <- intersect(nullmod$scanID, annot$sample.id[annot[[group.var]] == g])
-                resid.g <- nullmod$resid.marginal[nullmod$scanID %in% samp.g]
-                resid.norm <- rankNorm(resid.g)
-                ## rescale the inverse-normal residuals to have study-specific variances =
-                ## kinship variance component + study-specific residual
-                if (config["rescale_variance"] == "varcomp") {
-                    resid.scale <- nullmod$varComp["V_A"] + nullmod$varComp[paste0("V_", g)]
-                    resid.norm <- resid.norm * sqrt(resid.scale)
-                } else if (config["rescale_variance"] == "marginal") {
-                    resid.scale <- var(resid.g)
-                    resid.norm <- resid.norm * sqrt(resid.scale)
-                }
-                data.frame(sample.id=samp.g, resid.norm, stringsAsFactors=FALSE)
-            }))
-            annot$resid.norm <- resid.group$resid.norm[match(annot$sample.id, resid.group$sample.id)]
-        }
+## if we need an inverse normal transform, take residuals and refit null model
+if (as.logical(config["inverse_normal"])) {
+    if (is.null(group.var)) {
+        group.idx <- NULL
+        rankNorm.option <- "all"
+        rescale <- "None"
+    } else {
+        group.idx <- genesis2:::.indexList(annot[[group.var]])
+        rankNorm.option <- "by.group"
         
-        ## fit null model again with these residuals as outcome and allowing heterogeneous variances
-        resid.covars <- if (config["resid_covars"]) covars else NULL
-        nullmod <- fitNullMM(annot, outcome="resid.norm", covars=resid.covars,
-                             covMatList=grm, scan.include=sample.id,
-                             family=family, group.var=group.var)
-    }
-} else {
-    message("No kinship file specified, assuming samples are unrelated.")
-
-    nullmod <- fitNullReg(annot, outcome=outcome, covars=covars,
-                          scan.include=sample.id, family=family)
-
-    if (as.logical(config["inverse_normal"])) {
-        annot <- addInvNorm(annot, nullmod, outcome, covars)
-        resid.covars <- if (config["resid_covars"]) covars else NULL
-        nullmod <- fitNullReg(annot, outcome="resid.norm", covars=resid.covars,
-                              scan.include=sample.id, family=family)
+        if (config["rescale_variance"] == "varcomp") {
+            rescale <- "model"
+        } else if (config["rescale_variance"] == "marginal") {
+            rescale <- "residSD"
+        }
     }
     
+    nullmod <- updateNullModelOutcome(nullmod,
+                                      covMatList=grm,
+                                      group.idx=group.idx,
+                                      rankNorm.option=rankNorm.option,
+                                      rescale=rescale)
 }
 
 save(nullmod, file=config["out_file"])
