9781a7dd75f38c62cc49f2d5dfd4cefceb37b4f1
Stephanie M. Gogarten <sdmorris@uw.edu>
2019-03-04 13:50:32 -0800

    allow selection of samples in making BED file

--- R/gds2bed.R
+++ R/gds2bed.R
@@ -6,14 +6,14 @@ sessionInfo()
 
 argp <- arg_parser("Convert GDS to BED")
 argp <- add_argument(argp, "config", help="path to config file")
-argp <- add_argument(argp, "--chromosome", help="chromosome (1-24 or X,Y)", type="character")
 argp <- add_argument(argp, "--version", help="pipeline version number")
 argv <- parse_args(argp)
 cat(">>> TopmedPipeline version ", argv$version, "\n")
 config <- readConfig(argv$config)
 
 required <- c("gds_file", "bed_file")
-optional <- c("variant_include_file"=NA)
+optional <- c("sample_include_file"=NA,
+              "variant_include_file"=NA)
 config <- setConfigDefaults(config, required, optional)
 print(config)
 
@@ -21,6 +21,11 @@ gds <- seqOpen(config["gds_file"])
 
 if (!is.na(config["variant_include_file"])) {
     filterByFile(seqData, config["variant_include_file"])
+}
+
+if (!is.na(config["sample_include_file"])) {
+    sample.id <- getobj(config["sample_include_file"])
+    seqSetFilter(gds, sample.id=sample.id)
 }
 
 snpfile <- tempfile()
