6e11de25c58b254b6a66b3988c75e2b159680ece
roy <rollinforall@gmail.com>
2019-03-07 20:29:57 -0800

    Cleanup not needed install - done in devel

--- install_packages.R
+++ install_packages.R
@@ -10,10 +10,5 @@ install.packages(c("BiocManager", "remotes"), repos="https://cloud.r-project.org
 BiocManager::install(c("SeqVarTools", "SNPRelate", "GENESIS", "survey", "CompQuadForm",
                        "argparser", "ggplot2", "GGally", "rmarkdown"),
                      ask=FALSE)
-
-remotes::install_git("git://github.com/zhengxwen/SeqArray", dependencies=FALSE)
-remotes::install_git("git://github.com/smgogarten/GWASTools", dependencies=FALSE)
-remotes::install_git("git://github.com/smgogarten/SeqVarTools", dependencies=FALSE)
-remotes::install_git("git://github.com/UW-GAC/GENESIS", dependencies=FALSE)
-
+                     
 remotes::install_local("TopmedPipeline", dependencies=FALSE)
