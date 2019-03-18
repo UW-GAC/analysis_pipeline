695ef69ee4fdfdd6440e6013739fbd6af50175ee
Stephanie M. Gogarten <sdmorris@uw.edu>
2019-03-18 12:58:07 -0700

    install specific commit of SeqArray

--- install_packages.R
+++ install_packages.R
@@ -10,10 +10,16 @@ install.packages(c("BiocManager", "remotes"), repos="https://cloud.r-project.org
 BiocManager::install(c("SeqVarTools", "SNPRelate", "GENESIS", "survey", "CompQuadForm",
                        "argparser", "ggplot2", "GGally", "rmarkdown"),
                      ask=FALSE)
-                     
-remotes::install_git("git://github.com/zhengxwen/SeqArray", dependencies=FALSE)
-remotes::install_git("git://github.com/smgogarten/GWASTools", dependencies=FALSE)
-remotes::install_git("git://github.com/smgogarten/SeqVarTools", dependencies=FALSE)
-remotes::install_git("git://github.com/UW-GAC/GENESIS", dependencies=FALSE)
+
+
+#remotes::install_git("git://github.com/zhengxwen/SeqArray.git", dependencies=FALSE)
+#remotes::install_github("zhengxwen/SeqArray",  ref="31b9556", dependencies=FALSE)
+download.file("https://github.com/zhengxwen/SeqArray/archive/31b955666380fe1f92b5e2f2f56789a358c2cb77.zip",
+              destfile="SeqArray.zip", method="libcurl")
+system("unzip SeqArray.zip")
+install.packages("SeqArray-31b955666380fe1f92b5e2f2f56789a358c2cb77", repos=NULL, type="source")
+remotes::install_git("git://github.com/smgogarten/GWASTools.git", dependencies=FALSE)
+remotes::install_git("git://github.com/smgogarten/SeqVarTools.git", dependencies=FALSE)
+remotes::install_git("git://github.com/UW-GAC/GENESIS.git", dependencies=FALSE)
 
 remotes::install_local("TopmedPipeline", dependencies=FALSE)
