e71d05a7c510cf4b2ee4532b1d4be0b94c099328
roy <rollinforall@gmail.com>
2019-04-23 13:50:37 -0700

    More robust error reporting

--- run_python.sh
+++ run_python.sh
@@ -6,14 +6,7 @@
 #   n. last python argument
 f () {
     errcode=$? # save the exit code as the first thing done in the trap function
-    echo "error $errorcode"
-    echo "the command executing at the time of the error was"
-    echo "$BASH_COMMAND"
-    echo "on line ${BASH_LINENO[0]}"
-    # do some error handling, cleanup, logging, notification
-    # $BASH_COMMAND contains the command that was being executed at the time of the trap
-    # ${BASH_LINENO[0]} contains the line number in the script of that command
-    # exit the script or return to try again, etc.
+    echo "run_python: error executing python script $script - error code $errcode"
     exit $errcode  # or use some other value or do return instead
 }
 trap f ERR
