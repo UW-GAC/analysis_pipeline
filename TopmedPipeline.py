e6eda01ce83f4d1f900732a5eedfdd5ed9872060
roy <rollinforall@gmail.com>
2019-07-10 12:13:30 -0700

    Fix typo bug --mem_limit to --mem for slurm

--- TopmedPipeline.py
+++ TopmedPipeline.py
@@ -771,14 +771,14 @@ class Slurm_Cluster(Cluster):
         lmsg = lmsg + " /cores: " + lmsg_cores
 
         # get memory limit option
-        key = "memory_limits"
+        key = "--mem"
         lmsg_mem = "not provided"
         if key in self.clusterCfg.keys():
             memlim = super(Slurm_Cluster, self).memoryLimit(kwargs["job_name"])
             if memlim == None:
                 memlim = 8
-            submitOpts["--mem"] = str(memlim) + "M"
-            lmsg_mem = submitOpts["--mem_limit"]
+            submitOpts[key] = str(memlim) + "M"
+            lmsg_mem = submitOpts[key]
         lmsg = lmsg + " /memlim: " + lmsg_mem
 
         # get partition
