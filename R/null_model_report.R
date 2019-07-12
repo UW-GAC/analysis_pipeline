686b8cef95ad88cfef7ea5e727d64a5c35b39496
Adrienne Stilp <amstilp@uw.edu>
2019-07-12 10:30:09 -0700

    Render the null model report in the pipeline
    Update the null_model_report.R script to render the newly-added
    skeleton Rmd template.

--- R/null_model_report.R
+++ R/null_model_report.R
@@ -2,4 +2,16 @@ library(argparser)
 library(TopmedPipeline)
 sessionInfo()
 
-# Skeleton script for rendering a null model report.
+argp <- arg_parser("Analysis report")
+argp <- add_argument(argp, "config", help="path to config file")
+argp <- add_argument(argp, "--version", help="pipeline version number")
+argv <- parse_args(argp)
+cat(">>> TopmedPipeline version ", argv$version, "\n")
+config <- readConfig(argv$config)
+
+required <- c()
+optional <- c("out_file"="null_model_report")
+config <- setConfigDefaults(config, required, optional)
+print(config)
+
+custom_render_markdown("null_model_report", config["out_file"])
