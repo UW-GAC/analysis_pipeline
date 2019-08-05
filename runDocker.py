9dcb94331c279c09912b54066e1799fd62ac9947
roy <rollinforall@gmail.com>
2019-08-04 22:54:28 -0700

    slurm:Add support for cost estimation

--- runDocker.py
+++ runDocker.py
@@ -109,7 +109,8 @@ def Summary(hdr):
     print("\tVerbose: " + str(verbose))
     print("\tTest: " + str(test))
     print("\tDocker sdk installed? " + str(dockersdk))
-
+# start the timer
+startTime = timer.time()
 # command line parser
 parser = ArgumentParser(description = "Via docker sdk, create a docker container to execute an analysis job")
 # docker container run: image and command
@@ -127,6 +128,8 @@ parser.add_argument( "--mem_limit",
                      help = "Max memory (g) of container [default: unlimited]" )
 parser.add_argument("--working_dir", help = "working directory [default: current working directory]")
 # submit options explicitly passed (not available from env variable)
+parser.add_argument("--machine", help = "machine type of partition")
+parser.add_argument("--cost", help "hourly cost of machine type")
 parser.add_argument( "--verbose", action="store_true", default = False,
                      help = "Verbose output [default: False]")
 parser.add_argument( "--test", action="store_true", default = False,
@@ -143,6 +146,8 @@ mem_limit = args.mem_limit
 working_dir = args.working_dir
 if working_dir == None:
     working_dir = os.getenv('PWD')
+machine = args.machine
+cost = args.cost
 verbose = args.verbose
 test = args.test
 # get the slurm environment vars
@@ -211,6 +216,8 @@ if test:
 else:
     if dockersdk:
         pDebug("Running docker container ...")
+        if machine != None:
+            pInfo("Machine type: " + machine + " ( $" + str(cost) + "/hr )")
         if dockersdk:
             try:
                 client = docker.from_env()
@@ -225,6 +232,12 @@ else:
             pError("Docker sdk not installed.")
             sys.exit(2)
         pInfo("Docker run completed.")
+        if cost != None:
+            eTime = time.time() - startTime
+            eTimeHr = eTime/60./60.
+            totalCost = eTimeHr*cost)
+            pInfo("Elapsed time (hr): " + str(eTimeHr))
+            pInfo("Estimated cost: " + "$" + str(totalCost))
     else:
         pInfo("Docker sdk not installed; cannot run docker.")
         sys.exit(2)
