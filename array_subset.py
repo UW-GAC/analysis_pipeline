6c1eaa92e81a744ed61df9692b949d97611e780d
roy <rollinforall@gmail.com>
2020-01-06 17:01:19 -0800

    change header /usr/bin/env to python3

--- array_subset.py
+++ array_subset.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2.7
+#! /usr/bin/env python3
 
 """Subset sequencing GDS files for array concordance"""
 
--- vcf_subset.py
+++ vcf_subset.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python2.7
+#! /usr/bin/env python3
 
 """Generate subset of VCF files based on sample list"""
 
