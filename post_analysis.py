5d4fb62d5ad1dc33b1b81903a8f73c166a09d207
roy <rollinforall@gmail.com>
2020-02-03 17:43:57 -0800

    all: move files even if there were errors

--- post_analysis.py
+++ post_analysis.py
@@ -28,8 +28,34 @@ args = parser.parse_args()
 analysis = args.analysis
 starttime = args.starttime
 logfile = args.logfile
+# update the analysis log file (cost if in slurm)
 # strip of any leading/trailing quotes
 starttime = starttime.replace("_", " ")
+# end time (utc)
+tFmt = "%a, %d %b %Y %I:%M:%S %p"
+endtime = datetime.datetime.utcnow().strftime(tFmt)
+deltaTime = (datetime.datetime.strptime(endtime, tFmt) -
+             datetime.datetime.strptime(starttime, tFmt)).total_seconds()
+td_hrs = deltaTime/60./60.
+# append to log file
+with open(logfile, "a") as afile:
+    afile.write(analysis + " end time: " + endtime + "\n")
+    afile.write(analysis + " elapse time(hrs): " + str(td_hrs) + "\n")
+# get cost (for batch and slurm)
+totCost = 0.0;
+for file in os.listdir('.'):
+    if file.endswith(".log"):
+        with open(file) as thefile:
+            for line in thefile:
+                if line.startswith(">>> Info") and line.lower().find("cost"):
+                    ll = line.split("$")
+                    if len(ll) == 2:
+                        if isnumber(ll[1]):
+                            totCost += float(ll[1])
+if totCost != 0.0:
+    with open(logfile, "a") as afile:
+        afile.write(analysis + " total estimated cost: $" + str(round(totCost,2)) + "\n")
+
 # check for any errors in the log and resume files
 foundErr = False
 fileError = []
@@ -62,40 +88,14 @@ if len(ffiles):
     fileError.append(",".join(ffiles))
     foundErr = True;
 if foundErr:
-    print("post_analysis not executed because analysis error(s) has been found in " + str(fileError))
-    sys.exit(2)
-# end time (utc)
-tFmt = "%a, %d %b %Y %I:%M:%S %p"
-endtime = datetime.datetime.utcnow().strftime(tFmt)
-deltaTime = (datetime.datetime.strptime(endtime, tFmt) -
-             datetime.datetime.strptime(starttime, tFmt)).total_seconds()
-td_hrs = deltaTime/60./60.
-# append to log file
-with open(logfile, "a") as afile:
-    afile.write(analysis + " end time: " + endtime + "\n")
-    afile.write(analysis + " elapse time(hrs): " + str(td_hrs) + "\n")
-# get cost (for batch and slurm)
-totCost = 0.0;
-for file in os.listdir('.'):
-    if file.endswith(".log"):
-        with open(file) as thefile:
-            for line in thefile:
-                if line.startswith(">>> Info") and line.lower().find("cost"):
-                    ll = line.split("$")
-                    if len(ll) == 2:
-                        if isnumber(ll[1]):
-                            totCost += float(ll[1])
-if totCost != 0.0:
+    emsg = "post_analysis found some potential errors.  See the files: " + str(fileError)
+    print(">>> Warning: " + emsg)
+    # append to log file
     with open(logfile, "a") as afile:
-        afile.write(analysis + " total estimated cost: $" + str(round(totCost,2)) + "\n")
+        afile.write(">>> Warning: " + emsg + "\n")
 
 # cleanup
 errcnt = 0
-# delete all the resume related files
-rfiles = glob.glob("./resume/*")
-if len(rfiles):
-    for file in rfiles:
-        os.remove(file)
 
 for file in os.listdir('.'):
     try:
@@ -110,9 +110,9 @@ for file in os.listdir('.'):
         else:
             continue
     except Exception as e:
-        print('Error moving/deleting file ' + file + " (" + str(e) + ")")
+        print('>>> Error moving/deleting file ' + file + " (" + str(e) + ")")
         errcnt += 1
         continue
 if errcnt:
-    print("post_analysis encountered " + str(errcnt) + " errors.")
+    print(">>> Post_analysis encountered " + str(errcnt) + " errors.")
     sys.exit(2)
