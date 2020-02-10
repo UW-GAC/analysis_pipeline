1ae03d38f01b8895b1bfd69749ce4e40cca19e6d
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-02-10 14:21:53 -0800

    split king into two scripts for ibdseg and robust
    For large sample sizes, will no longer run KING robust. Split
    SNPRelate KING code into separate script for possible future use
    on smaller sample sizes. king.py will only run ibdseg.

--- king_robust.py
+++ king_robust.py
@@ -10,9 +10,7 @@ from copy import deepcopy
 
 description = """
 Identity by Descent with the following steps:
-1) Convert GDS to BED for use with KING
-2) KING --ibdseg to get initial kinship estimates
-3) Convert KING output to sparse matrix
+1) KING robust using SNPRelate algorithm
 """
 
 parser = ArgumentParser(description=description)
@@ -57,45 +55,16 @@ configdict = TopmedPipeline.directorySetup(configdict, subdirs=["config", "data"
 cluster.analysisInit(print_only=print_only)
 
 
-job = "gds2bed"
+job = "ibd_king"
 
 rscript = os.path.join(pipeline, "R", job + ".R")
 
-# include all samples in BED
 config = deepcopy(configdict)
-config["sample_include_file"] = "NA"
+config["out_file"] = configdict["data_prefix"] + "_king_robust.gds"
 configfile = configdict["config_prefix"] + "_" + job + ".config"
 TopmedPipeline.writeConfig(config, configfile)
-jobid = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile, version], email=email, print_only=print_only)
 
-
-## this swaps alleles to make KING --ibdseg happy about reading the file
-job = "plink_make-bed"
-
-bedprefix = configdict["bed_file"]
-arglist = ["--bfile", bedprefix, "--make-bed", "--out", bedprefix]
-
-plinkid = cluster.submitJob(binary=True, job_name=job, cmd="plink", args=arglist, holdid=[jobid], email=email, print_only=print_only)
-
-## when input and output files have same name, plink renames input with "~"
-tmpid = cluster.submitJob(binary=True, job_name="rm_tmp_files", cmd="rm", args=[bedprefix + ".*~"], holdid=[plinkid], email=email, print_only=print_only)
-
-
-
-job = "king_ibdseg"
-
-bedfile = configdict["bed_file"] + ".bed"
-outprefix = configdict["data_prefix"] + "_king_ibdseg"
-arglist = ["-b", bedfile, "--cpus", ncores, "--ibdseg", "--prefix", outprefix]
-
-segid = cluster.submitJob(binary=True, job_name=job, cmd="king", args=arglist, holdid=[plinkid], request_cores=ncores, email=email, print_only=print_only)
-
-kingfile = outprefix + ".seg"
-
-
-# gzip output
-segid = cluster.submitJob(binary=True, job_name="gzip", cmd="gzip", args=[kingfile], holdid=[segid], email=email, print_only=print_only)
-kingfile = kingfile + ".gz"
+kinid = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile, version], holdid=[jobid], request_cores=ncores, email=email, print_only=print_only)
 
 
 job = "kinship_plots"
@@ -103,29 +72,14 @@ job = "kinship_plots"
 rscript = os.path.join(pipeline, "R", job + ".R")
 
 config = deepcopy(configdict)
-config["kinship_file"] = kingfile
-config["kinship_method"] = "king_ibdseg"
-config["out_file_all"] = configdict["plots_prefix"] + "_king_ibdseg_kinship_all.pdf"
-config["out_file_cross"] = configdict["plots_prefix"] + "_king_ibdseg_kinship_cross.pdf"
-config["out_file_study"] = configdict["plots_prefix"] + "_king_ibdseg_kinship_study.pdf"
-configfile = configdict["config_prefix"] + "_" + job + "_ibdseg.config"
+config["kinship_file"] = configdict["data_prefix"] + "_king_robust.gds"
+config["kinship_method"] = "king"
+config["out_file_all"] = configdict["plots_prefix"] + "_king_robust_kinship_all.pdf"
+config["out_file_study"] = configdict["plots_prefix"] + "_king_robust_kinship_study.pdf"
+configfile = configdict["config_prefix"] + "_" + job + "_robust.config"
 TopmedPipeline.writeConfig(config, configfile)
 
-segplotid = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile, version], holdid=[segid], email=email, print_only=print_only)
-
-
-job = "king_to_matrix"
-
-rscript = os.path.join(pipeline, "R", job + ".R")
-
-config = deepcopy(configdict)
-config["king_file"] = kingfile
-config["kinship_method"] = "king_ibdseg"
-config["out_prefix"] = configdict["data_prefix"] + "_king_ibdseg_Matrix"
-configfile = configdict["config_prefix"] + "_" + job + "_ibdseg.config"
-TopmedPipeline.writeConfig(config, configfile)
-
-segmatid = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile, version], holdid=[segid], email=email, print_only=print_only)
+kinid = cluster.submitJob(job_name=job, cmd=driver, args=[rscript, configfile, version], holdid=[kinid], email=email, print_only=print_only)
 
 
 # post analysis
@@ -133,7 +87,7 @@ bname = "post_analysis"
 job = "king" + "_" + bname
 jobpy = bname + ".py"
 pcmd=os.path.join(submitPath, jobpy)
-holdlist = [segplotid, segmatid]
+holdlist = [kinid]
 argList = ["-a", cluster.getAnalysisName(), "-l", cluster.getAnalysisLog(),
            "-s", cluster.getAnalysisStartSec()]
 cluster.submitJob(binary=True, job_name=job, cmd=pcmd, args=argList,
