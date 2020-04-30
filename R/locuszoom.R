b78fa1012e2ef0294e88e559d5641fd933d21431
Matt <mconomos@uw.edu>
2020-04-30 14:42:50 -0700

    wip

--- R/locuszoom.R
+++ R/locuszoom.R
@@ -3,8 +3,6 @@ library(TopmedPipeline)
 library(SeqVarTools)
 library(dplyr)
 sessionInfo()
-.libPaths()
-print(writeMETAL)
 
 argp <- arg_parser("LocusZoom plots")
 argp <- add_argument(argp, "config", help="path to config file")
