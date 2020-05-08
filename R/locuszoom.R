0df596fc073c843018997a34041e0d0566a92525
Matt <mconomos@uw.edu>
2020-05-07 17:00:51 -0700

    wip

--- R/locuszoom.R
+++ R/locuszoom.R
@@ -206,9 +206,9 @@ command <- paste("locuszoom",
                  "--prefix ", prefix,
                  paste0("signifLine=\"", -log10(as.numeric(config["signif_level"])), "\" signifLineColor=\"gray\" signifLineWidth=\"2\""),
                  "ylab=\"-log10(p-value)\"",
-                 "refsnpLineColor=\"goldenrod\"",
+                 "refsnpLineColor=\"red\"",
                  "refsnpLineWidth=\"2\"",
-                 "refsnpTextSize=\"0.9\"",
+                 "refsnpTextSize=\"0.95\"",
                  paste0("rfrows=\"", as.numeric(config["gene_rows"]), "\""))
 
 if(!is.null(title)){
