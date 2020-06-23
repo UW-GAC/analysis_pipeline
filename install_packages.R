a68e53908c314085be44fa2da5767a63dc88288a
Stephanie M. Gogarten <sdmorris@uw.edu>
2020-06-23 15:34:57 -0700

    use latest version of SeqVarTools

--- install_packages.R
+++ install_packages.R
@@ -17,9 +17,9 @@ if (getRversion() < "3.6.0") {
     remotes::install_git("git://github.com/zhengxwen/gdsfmt.git", ref="v1.20.0", dependencies=FALSE)
     remotes::install_git("git://github.com/zhengxwen/SeqArray.git", ref="v1.24.0", dependencies=FALSE)
     remotes::install_git("git://github.com/smgogarten/GWASTools.git", ref="v1.32.0", dependencies=FALSE)
-    remotes::install_git("git://github.com/smgogarten/SeqVarTools.git", ref="v1.23.1", dependencies=FALSE)
 }
 
+remotes::install_git("git://github.com/smgogarten/SeqVarTools.git", ref="v1.27.1", dependencies=FALSE)
 remotes::install_git("git://github.com/UW-GAC/GENESIS.git", ref="v2.19.1", dependencies=FALSE)
 
 remotes::install_local("TopmedPipeline", dependencies=FALSE)
