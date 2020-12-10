17660fcad4972b42b7e821ec98f67738a5bb798c
Adrienne Stilp <amstilp@uw.edu>
2020-12-10 09:20:36 -0800

    Update install_packages.R script for updated GENESIS version

--- install_packages.R
+++ install_packages.R
@@ -20,7 +20,6 @@ if (getRversion() < "3.6.0") {
 }
 
 remotes::install_git("git://github.com/smgogarten/SeqVarTools.git", ref="v1.27.1", dependencies=FALSE)
-remotes::install_git("git://github.com/UW-GAC/GENESIS.git", ref="v2.19.5", dependencies=FALSE)
+remotes::install_git("git://github.com/UW-GAC/GENESIS.git", ref="v2.21.1", dependencies=FALSE)
 
 remotes::install_local("TopmedPipeline", dependencies=FALSE)
-
